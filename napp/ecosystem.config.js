45ddacf1f08ffe29b4e3490bb358c895a97e1e1a
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-09-27 12:32:46 +0300

    add:

--- napp/ecosystem.config.js
+++ napp/ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'MainWelder',
-      port: '3001',
+      name: 'MainWelderKZ',
+      port: '3002',
       exec_mode: 'cluster',
       instances: 'max',
-      // script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs'
     }
   ]
 }
