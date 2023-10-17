b6c0aaf2b41bfaa6967addf00208da0303e49cda
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-10-17 09:06:40 +0300

    add:

--- dapp/catalog/management/commands/agent_sender.py
+++ dapp/catalog/management/commands/agent_sender.py
@@ -1,45 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from pathlib import Path
 from bot.bot import Bot
-from django.conf import settings
-import requests
-from main.conf import mailagent_bot_token, mail_contacts
-from django.template.loader import render_to_string
-from django.core.management.base import BaseCommand
+from main.conf import mailagent_bot_token
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 class Command(BaseCommand):
     args = ''
     help = ''
-    
+
     def handle(self, *args, **options):
         pass
 
 
-# mailagent_bot_token = "001.4217311500.1159573524:1002164570"
-# mail_contacts = {
-#     "admins": [
-#         "efimovsergeipr@mail.ru",
-#         ],
-#     "channel": [
-#         # "@AoLG-JRaQb24KBu3ZwI",
-#     ],
-# }
+target = "@AoLIhsdsR3MfM6vQHhg"
 
+bot = Bot(token=mailagent_bot_token)
 
-# bot = Bot(token=mailagent_bot_token)
-
-# print('hallo welt')
-# def send_alert_to_agent(order=None):
-    
-#     chats_to_send_notifications = mail_contacts['admins']
-#     text_to_send = render_to_string('magent.html', {'uuid': order['uuid'],})
-
-
-
-addr = 'https://agent.mail.ru/bot/v1/'
-method = '​/messages​/sendFile'
-
-url = 'http://127.0.0.1:8000/'
-myobj = {'somekey': 'somevalue'}
-
-response = requests.post(url, data = myobj)
-print(response.status_code)
+def image_cb():
+    bot.send_file(
+        chat_id= target,
+        file = open('./image.jpg', 'rb')
+    )
+image_cb()
