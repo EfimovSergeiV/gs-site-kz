85476ff94686ac60cbf1fb11b321e741b0cf4634
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-11-13 15:29:57 +0300

    add:

--- napp/components/AppFooter.vue
+++ napp/components/AppFooter.vue
@@ -11,40 +11,89 @@
 
       <div class="md:flex md:justify-between items-center py-2">
 
-        <div class="">
-          <ul class="grid grid-cols-1 gap-x-2 gap-y-2 text-xs md:text-base mt-3 sm:mt-0 md:order-last py-2 ">
-            <!-- <li>
-              <div class="group">
-                <nuxt-link :to="{ name: 'about' }" class="mr-4 md:mr-6 text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Производители</nuxt-link>
-                <div class="absolute bottom-0 left-0 translate-y-0 group-hover:-translate-y-4 py-4 w-full transition invisible group-hover:visible opacity-0 group-hover:opacity-100 duration-500 ease-in-out group-hover:transform z-40">
-                  <div class="bg-white border border-gray-500/50 rounded-md">
-                    <div class="flex flex-wrap gap-4 px-4 py-6">
-                      <div v-for="brand in brands" :key="brand.id" class="py-4">
-                        <nuxt-link class="text-gray-700 :hover:text-gray-900 text-xl font-semibold" :to="{ name: 'prods', query: { brnd: brand.id, page: 1 }}">{{ brand.brand }}</nuxt-link>
-                      </div>                      
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li> -->
-            <li>
-              <button @click="showAllert()" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Доставка</button>
-            </li>
-            
-            <li>
-              <button @click="showAllert()" class="mr-4 md:mr-6 text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Способы оплаты</button>
-            </li>
+        <div class="md:order-2">
+          <div class="flex gap-2 items-end justify-end">
+            <p class="mdi mdi-36px mdi-map-marker-outline"></p>
+            <div class="flex flex-col">
+              <p class="text-sm text-end">Рудный, ул. Топоркова 35, 111500</p>
+              <a href="tel:+77084238070" class="text-2xl text-end">+7 (708) 423-80-70</a>
+            </div>
+          </div>
 
-            <li>
-              <nuxt-link :to="{ name: 'policy'}" class="mr-4 md:mr-6 text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 text-xs md:text-base">Политика обработки данных</nuxt-link>
-            </li>
-          </ul>
+
+          <p class="py-2 text-sm">Наш график работы:</p>
+          <div class="flex gap-10">
+            <div class="grid grid-cols-1">
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">понедельник</p>
+                  <p class="text-xs">08:30 – 17:30</p>
+                </div>                      
+              </div>
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">вторник</p>
+                  <p class="text-xs">08:30 – 17:30</p>
+                </div>                      
+              </div>
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">среда</p>
+                  <p class="text-xs">08:30 – 17:30</p>
+                </div>                          
+              </div>
+            </div>
+            <div class="grid grid-cols-1">
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">четверг</p>
+                  <p class="text-xs">08:30 – 17:30</p>
+                </div>                      
+              </div>
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">пятница</p>
+                  <p class="text-xs">08:30 – 17:30</p>
+                </div>                      
+              </div>
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">суббота</p>
+                  <p class="text-xs">09:00 – 15:00</p>
+                </div>                      
+              </div>
+              <div class=" border-b border-gray-200 dark:border-gray-700">
+                <div class="grid grid-cols-2 gap-6">
+                  <p class="text-xs">воскресенье</p>
+                  <p class="text-xs">Закрыто</p>
+                </div>                          
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="w-full md:w-1/2">
+
+        <div class="w-full h-full md:w-1/2 md:order-1">
+          
           <p class="text-[10px] md:text-xs my-3 select-none text-gray-700 dark:text-gray-300">
             Все цены и материаллы, указанные на сайте www.glsvar.kz, приведены как справочная информация и не являются публичной офертой и могут быть изменены в любое время без предупреждения. 
             Для получения подробной информации о стоимости, сроках и условиях поставки просьба обращаться по указанным на сайте телефонам.
-          </p>          
+          </p>  
+          <div class="">
+            <ul class="flex justify-start gap-8 text-xs md:text-sm mt-3 sm:mt-0 md:order-last py-2 ">
+              <li>
+                <button @click="showAllert()" class="text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Доставка</button>
+              </li>
+              
+              <li>
+                <button @click="showAllert()" class=" text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100">Способы оплаты</button>
+              </li>
+
+              <li>
+                <nuxt-link :to="{ name: 'policy'}" class=" text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 text-xs md:text-sm">Политика обработки данных</nuxt-link>
+              </li>
+            </ul>
+          </div>
+        
         </div>
       </div>
 
--- napp/pages/about.vue
+++ napp/pages/about.vue
@@ -12,15 +12,6 @@
     { "id": 6, "url": "/shops/6.webp"},
   ]
 
-  const mapURLs = {
-    "Псков" : "https://yandex.ru/map-widget/v1/?um=constructor%3A47f57d246b40670e64b17cb3f82dde8a81f3b3a825fa0ec9dc5e2a360214ec38&amp;source=constructor",
-    "Москва" : "https://yandex.ru/map-widget/v1/?um=constructor%3Ae113235413b59c4ae31cb4d894caa9612e5c88e95cb5fc873f8508770800a19f&amp;source=constructor",
-    "Санкт-Петербург" : "https://yandex.ru/map-widget/v1/?um=constructor%3A4892b9a6ff89034d14a4833ed892e30ddac4cde58559e85c6ae75d9d5cb766b2&amp;source=constructor",
-    "Великие луки" : "https://yandex.ru/map-widget/v1/?um=constructor%3A8cbd01689857fcf3e2c9604637d5e0e3ac33b5e71467a701ed470d12b63e1dea&amp;source=constructor",
-    "Смоленск" : "https://yandex.ru/map-widget/v1/?um=constructor%3A22e3d619383ca4fb59e78352a52f5e3808418009918e23d73549578277b6a013&amp;source=constructor",
-    "Петрозаводск" : "https://yandex.ru/map-widget/v1/?um=constructor%3A7a6886838355b4c4aab4597dd2ee3dcd40eb7c6cd2bab5638666af5f1606c267&amp;source=constructor",
-  }
-
   const descriptionData = 'С 2009 года Главный сварщик – ваш надежный партнер в мире сварочного оборудования! '
 
   useSeoMeta({
