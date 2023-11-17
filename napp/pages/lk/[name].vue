9301e2537bac5ed649b16ffb5cb9a0501ed15e2c
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-11-17 14:40:06 +0300

    add:

--- napp/pages/lk/[name].vue
+++ napp/pages/lk/[name].vue
@@ -39,12 +39,12 @@
               </div>
               <div class="text-sm text-gray-100 rounded-lg bg-blue-600 hover:bg-blue-700 border border-gray-300/50 dark:border-gray-500/50 transition-all duration-1000">
                 <div class=" bg-gradient-to-br from-gray-100/20 to-gray-900/40 rounded-lg">
-                  <p class="text-white text-sm w-full text-center py-1.5">Товары в избранном</p>
+                  <p class="text-white text-sm w-full text-center py-1.5">Избранное</p>
                 </div>
               </div>
               <div class="text-sm text-gray-100 rounded-lg bg-blue-600 hover:bg-blue-700 border border-gray-300/50 dark:border-gray-500/50 transition-all duration-1000">
                 <div class=" bg-gradient-to-br from-gray-100/20 to-gray-900/40 rounded-lg">
-                  <p class="text-white text-sm w-full text-center py-1.5">Товары в сравнении</p>
+                  <p class="text-white text-sm w-full text-center py-1.5">В сравнении</p>
                 </div>
               </div>
             </div>
@@ -56,12 +56,29 @@
           </div>
 
 
-          <div class="px-4">
-            <p> Вы недавно смотрели</p>
+          <div class="px-4 py-2">
+            <p class="py-4"> Вы недавно смотрели</p>
 
             <div class="">
+              <Swiper
+                class=""
+                :modules="[]"
+                :slidesPerView="'auto'"
+                :loop="true"
+                :spaceBetween="10"
+              >
               
+                <SwiperSlide  v-for="product in productsStore.viewed" :key="product.id" class="">
+                  <ProductCard :product="product" />
+                </SwiperSlide>
+
+                <div class="absolute bottom-0 right-0 py-1 px-1 z-40">
+                  <SwiperControls class="bg-gray-100 rounded-full border border-gray-200 hover:border-gray-300 dark:border-gray-600 dark:hover:border-gray-500 dark:bg-gray-700 transition-all duration-500 px-1" />
+                </div>
+
+              </Swiper>
             </div>
+
 
           </div>
 
@@ -74,3 +91,9 @@
 
   </div>
 </template>
+
+<style scoped>
+.swiper-slide {
+  width: 260px;
+}
+</style>
