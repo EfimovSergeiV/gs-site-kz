3f0575c0a8b9083411648671eb46d4efc399bba3
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-12-07 15:25:23 +0300

    add:

--- napp/app.vue
+++ napp/app.vue
@@ -1,9 +1,11 @@
 
 <template>
-  <NuxtLayout>
+
+  <div>Главный сварщик</div>
+  <!-- <NuxtLayout>
     <NuxtLoadingIndicator :color="'repeating-linear-gradient(to right,#38bdf8 0% ,#0369a1 100%)'" :height="1" />
     <NuxtPage />
-  </NuxtLayout>
+  </NuxtLayout> -->
 </template>
 
 <style lang="postcss">
