a65b3aa4c0713d02125386b5e37dd196d43c7e98
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-12-13 12:45:24 +0300

    add:

--- napp/components/Recommend.vue
+++ napp/components/Recommend.vue
@@ -11,7 +11,7 @@
   <div class="container py-2 mx-auto px-4 max-w-6xl lg:px-8">
     <div class="p-2 bg-white rounded-md border dark:border-gray-700 dark:bg-gray-800 shadow-sm relative">
       <div class="">
-        <blockquote class="text-base lg:text-2xl font-semibold italic text-center text-gray-700 dark:text-gray-300">
+        <blockquote class="text-2xl font-semibold italic text-center text-gray-700 dark:text-gray-300">
           
           <span class="px-4">ЕСЛИ НУЖНО</span>
           <span class="before:block before:absolute before:-inset-2 before:-skew-y-3 before:bg-blue-600 relative inline-block my-4 lg:my-2">
