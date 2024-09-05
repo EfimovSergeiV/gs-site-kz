d1bfa2282e2c13bb2d47a93db03e23cb74ce3c5b
anon <efimovsergeiv@gmail.com>
2024-09-05 15:39:27 +0300

    add:

--- dapp/catalog/management/commands/opensearch-rebuild.py
+++ dapp/catalog/management/commands/opensearch-rebuild.py
@@ -15,7 +15,7 @@ auth = OPENSEARCH_DSL['default']
 
 # Конфигурация подключения к OpenSearch
 opensearch_client = OpenSearch(
-    hosts=[{'host': 'opensearch.glsvar.ru', 'port': 443}],
+    hosts=[{'host': 'search.glsvar.ru', 'port': 443}],
     http_auth=auth['http_auth'],
     use_ssl=True,
     verify_certs=True
